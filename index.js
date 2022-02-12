072f17c3c469878b928014fde52886e93cc06a1b
Littie6amer <58089255+Littie6amer@users.noreply.github.com>
2022-02-12 14:18:35 +0000

    Added a missing )

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new Discord.Client({
 client.on('ready', () => console.log(`Logged in as ${client.user.tag} (${client.user.id})`));
 
 client.on('messageCreate', async (message) => {
-  if (message.author.bot || !message.content.startsWith(config.prefix) return;
+  if (message.author.bot || !message.content.startsWith(config.prefix)) return;
   const command = message.content.slice(config.prefix.length).split(/ +/)[0]?.toLowerCase()
 
   console.log(`${message.author.tag} (${message.author.id}) ran command ${command}`);
